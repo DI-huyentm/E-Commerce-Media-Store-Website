e1dd29eceed16ec256cec70ce035de9c11cd5d98
DI-huyentm <huyen.cnh70@gmail.com>
2024-12-26 21:08:19 +0700

    resubmit first draft BE

--- BE/src/main/java/vn/hust/aims/controller/PaymentController.java
+++ BE/src/main/java/vn/hust/aims/controller/PaymentController.java
@@ -8,14 +8,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import vn.hust.aims.controller.dto.request.payment.PayOrderRequest;
-import vn.hust.aims.controller.dto.request.payment.paypal.PaypalTransactionRequest;
 import vn.hust.aims.controller.dto.request.payment.vnpay.VNPayTransactionRequest;
 import vn.hust.aims.controller.dto.response.payment.PayOrderResponse;
 import vn.hust.aims.response.AimsCommonResponse;
 import vn.hust.aims.service.PaymentService;
 import vn.hust.aims.service.PaymentTransactionService;
 import vn.hust.aims.service.dto.output.payment.PayOrderOutput;
-import vn.hust.aims.service.dto.output.payment.paypal.PaypalTransactionOutput;
 import vn.hust.aims.service.dto.output.payment.vnpay.VNPayTransactionOutput;
 import vn.hust.aims.utils.ResponseUtil;
 
@@ -56,12 +54,6 @@ public class PaymentController {
 
     return "redirect:" + output.getUrl() ;
 
-  }
-
-  @PostMapping("/paypal-return")
-  public String handlePayPalReturn(@RequestBody PaypalTransactionRequest request){
-    PaypalTransactionOutput output =  paymentTransactionService.savePaypalTransaction(request.toInput());
-    return output.getUrl();
   }
 }
 // Design principles - Kết quả kiểm tra các nguyên tắc liên quan:
