76c003aa1d4b4702f33ffa8c553f97d47e544fd1
DI-huyentm <huyen.cnh70@gmail.com>
2024-12-26 18:36:39 +0700

    submit first draft

--- BE/src/main/java/vn/hust/aims/response/AimsCommonResponse.java
+++ BE/src/main/java/vn/hust/aims/response/AimsCommonResponse.java
@@ -12,57 +12,57 @@ import vn.hust.aims.exception.ErrorCodeList;
 @NoArgsConstructor
 public class AimsCommonResponse<T> implements Serializable {
 
-    private static final long serialVersionUID = -6669686067446636607L;
+  private static final long serialVersionUID = -6669686067446636607L;
 
-    protected String code;
+  protected String code;
 
-    protected String message;
+  protected String message;
 
-    protected T result;
+  protected T result;
 
-    public AimsCommonResponse(T result) {
-        this.code = Constant.SUCCESS_CODE;
-        this.message = Constant.SUCCESS_MESSAGE;
-        this.result = result;
-    }
+  public AimsCommonResponse(T result) {
+    this.code = Constant.SUCCESS_CODE;
+    this.message = Constant.SUCCESS_MESSAGE;
+    this.result = result;
+  }
 
-    public static AimsCommonResponse<Object> internalError() {
-        return new AimsCommonResponse<>(
-            Constant.INTERNAL_SERVER_ERROR_CODE,
-            Constant.INTERNAL_SERVER_ERROR_MESSAGE,
-            null
-        );
-    }
+  public static AimsCommonResponse<Object> internalError() {
+    return new AimsCommonResponse<>(
+        Constant.INTERNAL_SERVER_ERROR_CODE,
+        Constant.INTERNAL_SERVER_ERROR_MESSAGE,
+        null
+    );
+  }
 
-    public static AimsCommonResponse<Object> badRequest(String message) {
-        return new AimsCommonResponse<>(
-            HttpStatus.BAD_REQUEST.toString(),
-            message,
-            null
-        );
-    }
+  public static AimsCommonResponse<Object> badRequest(String message) {
+    return new AimsCommonResponse<>(
+        HttpStatus.BAD_REQUEST.toString(),
+        message,
+        null
+    );
+  }
 
-    public static AimsCommonResponse<Object> badRequest(String message, ErrorCodeList errorCode) {
-        return new AimsCommonResponse<>(
-            errorCode.toCode(),
-            message,
-            null
-        );
-    }
+  public static AimsCommonResponse<Object> badRequest(String message, ErrorCodeList errorCode) {
+    return new AimsCommonResponse<>(
+        errorCode.toCode(),
+        message,
+        null
+    );
+  }
 
-    public static AimsCommonResponse<Object> badRequest(ErrorCodeList errorCode, Object data) {
-        return new AimsCommonResponse<>(
-            errorCode.toCode(),
-            errorCode.toString(),
-            data
-        );
-    }
+  public static AimsCommonResponse<Object> badRequest(ErrorCodeList errorCode, Object data) {
+    return new AimsCommonResponse<>(
+        errorCode.toCode(),
+        errorCode.toString(),
+        data
+    );
+  }
 
-    public static AimsCommonResponse<Object> forbidden(String message) {
-        return new AimsCommonResponse<>(
-            HttpStatus.FORBIDDEN.toString(),
-            message,
-            null
-        );
-    }
+  public static AimsCommonResponse<Object> forbidden(String message) {
+    return new AimsCommonResponse<>(
+        HttpStatus.FORBIDDEN.toString(),
+        message,
+        null
+    );
+  }
 }
