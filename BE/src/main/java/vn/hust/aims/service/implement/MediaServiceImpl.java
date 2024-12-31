cce13739068f52226274eab8afd831a30a9acd80
DI-huyentm <huyen.cnh70@gmail.com>
2024-12-31 16:57:34 +0700

    remove redundant function for media

--- BE/src/main/java/vn/hust/aims/service/implement/MediaServiceImpl.java
+++ BE/src/main/java/vn/hust/aims/service/implement/MediaServiceImpl.java
@@ -32,16 +32,10 @@ import vn.hust.aims.exception.UpdateDeleteLimitExceededException;
 import vn.hust.aims.repository.media.ChangelogRepository;
 import vn.hust.aims.repository.media.MediaRepository;
 import vn.hust.aims.service.MediaService;
-import vn.hust.aims.service.dto.input.media.CreateMediaInput;
-import vn.hust.aims.service.dto.input.media.DeleteMediaBulkInput;
 import vn.hust.aims.service.dto.input.media.GetAllMediaInput;
 import vn.hust.aims.service.dto.input.media.GetMediaInput;
-import vn.hust.aims.service.dto.input.media.UpdateMediaInput;
-import vn.hust.aims.service.dto.output.media.CreateMediaOutput;
-import vn.hust.aims.service.dto.output.media.DeleteMediaBulkOutput;
 import vn.hust.aims.service.dto.output.media.GetAllMediaOutput;
 import vn.hust.aims.service.dto.output.media.GetMediaOutput;
-import vn.hust.aims.service.dto.output.media.UpdateMediaOutput;
 import vn.hust.aims.utils.JsonMapper;
 
 @Service
@@ -50,15 +44,6 @@ public class MediaServiceImpl implements MediaService {
   private final MediaRepository mediaRepository;
   private final ChangelogRepository changelogRepository;
   private final ResourceLoader resourceLoader;
-
-  public CreateMediaOutput createMedia(CreateMediaInput createMediaInput) {
-    MediaFactoryInterface mediaFactoryInterface = MediaFactoryBuilder.get(createMediaInput.getMediaType());
-
-    Media media = mediaFactoryInterface.build(JsonMapper.writeValueAsString(createMediaInput.getMediaInfo()));
-
-    mediaRepository.save(media);
-    return CreateMediaOutput.from("Create success");
-  }
 
   public GetMediaOutput getMedia(GetMediaInput getMediaInput) {
     Media media = mediaRepository.findById(getMediaInput.getId()).orElseThrow(
@@ -90,101 +75,6 @@ public class MediaServiceImpl implements MediaService {
     }
 
     return GetAllMediaOutput.builder().mediaPage(mediaPage).build();
-  }
-
-  public DeleteMediaBulkOutput deleteBulkMedia(DeleteMediaBulkInput deleteMediaBulkInput) {
-    if (deleteMediaBulkInput.getIds().isEmpty()) {
-      mediaRepository.deleteAll();
-      return DeleteMediaBulkOutput.from("Delete all success");
-    }
-
-    Instant now = Instant.now();
-
-    List<Changelog> listChangelog = changelogRepository.findAllByAuthorIdAndTimestampAfter(deleteMediaBulkInput.getAuthorId(), now.minus(1, ChronoUnit.DAYS));
-
-    if (deleteMediaBulkInput.getIds().size() + listChangelog.size() > 30) {
-      throw new UpdateDeleteLimitExceededException();
-    }
-
-    for (Long id: deleteMediaBulkInput.getIds()) {
-      Changelog changelog = Changelog.builder()
-          .timestamp(now)
-          .authorId(deleteMediaBulkInput.getAuthorId())
-          .changedMediaId(id)
-          .isPriceChange(0)
-          .build();
-
-      changelogRepository.save(changelog);
-    }
-
-    mediaRepository.deleteAllById(deleteMediaBulkInput.getIds());
-
-    return DeleteMediaBulkOutput.from("Delete bulk success");
-  }
-
-  public UpdateMediaOutput updateMedia(UpdateMediaInput updateMediaInput) {
-    Long id = updateMediaInput.getMediaId();
-
-    Media mediaEntity = mediaRepository.getReferenceById(id);
-
-    MediaFactoryInterface mediaFactoryInterface = MediaFactoryBuilder.get(MediaType.from(mediaEntity.getCategory()));
-
-    updateMediaInput.getMediaInfo().put("createdAt", mediaEntity.getCreatedAt());
-
-    Media toUpdate = mediaFactoryInterface.build(JsonMapper.writeValueAsString(updateMediaInput.getMediaInfo()));
-    toUpdate.setId(id);
-
-    Instant now = Instant.now();
-
-    Changelog changelog = Changelog.builder()
-        .changedMediaId(id)
-        .timestamp(now)
-        .build();
-
-    List<Changelog> listChangelog = changelogRepository.findAllByAuthorIdAndTimestampAfter(updateMediaInput.getAuthorId(), now.minus(1, ChronoUnit.DAYS));
-
-    if (listChangelog.size() > 30) {
-      throw new UpdateDeleteLimitExceededException();
-    } else {
-      if (!Objects.equals(mediaEntity.getPrice(), toUpdate.getPrice())) {
-        changelog.setIsPriceChange(1);
-
-        List<Changelog> listPriceChange = changelogRepository.findAllByChangedMediaIdAndTimestampAfterAndIsPriceChange(id, now.minus(1, ChronoUnit.DAYS), 1);
-        if (listPriceChange.size() > 1) {
-          throw new PriceChangeLimitExceededException();
-        }
-      } else {
-        changelog.setIsPriceChange(0);
-      }
-    }
-
-    changelogRepository.save(changelog);
-
-    System.out.println(toUpdate);
-
-    mediaRepository.save(toUpdate);
-
-    return UpdateMediaOutput.from("Update success");
-  }
-
-  public String createMediaImage(MultipartFile file) {
-    try {
-      String filename = UUID.randomUUID() + "_" + file.getOriginalFilename();
-
-      // Get the resource directory
-      Resource resourceDir = resourceLoader.getResource("classpath:images/");
-
-      // Create the path for the new file
-      Path filePath = Paths.get(resourceDir.getURI()).resolve(filename);
-
-      // Write the file content to the specified path
-      Files.write(filePath, file.getBytes());
-
-      return filename;
-    } catch (IOException e) {
-      // Handle exceptions accordingly, e.g., log or throw a custom exception
-      throw new RuntimeException("Failed to upload image", e);
-    }
   }
 
   public byte[] getMediaImage(String imageName) {
